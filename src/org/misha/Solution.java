0bc56df5db33f51a86972c7f44a3aba4370c0128
misha <shma2001@gmail.com>
2017-09-02 17:44:43 +0600

    --

--- src/org/misha/Solution.java
+++ src/org/misha/Solution.java
@@ -8,31 +8,20 @@ class Solution {
     private static final int LOWER = -25000;
 
     private int solution(int[] A) {
-        if (max(A) > UPPER || min(A) < LOWER) {
-            return -1;
-        }
+        if (max(A) > UPPER || min(A) < LOWER) return -1;
         int length = A.length;
         int[] s = new int[length];
         int sum = 0;
-        for (int i = 0; i < length; ++i) {
-            sum += A[i];
-            s[i] = sum;
-        }
-        if (sum == 0) {
-            return 0;
-        }
+        for (int i = 0; i < length; sum += A[i], s[i] = sum, ++i);
+        if (sum == 0) return 0;
         for (int i = 0; i < length - 1; ++i) {
-            if (s[i] == sum - s[i + 1]) {
-                return i + 1;
-            }
+            if (s[i] == sum - s[i + 1]) return i + 1;
         }
         return -1;
     }
 
     private int max(int[] x) {
-        if (x.length == 0) {
-            return Integer.MAX_VALUE;
-        }
+        if (x.length == 0) return Integer.MAX_VALUE;
         int result = x[0];
         for (int a : x) {
             if (a > result) {
@@ -44,13 +33,8 @@ class Solution {
 
     private int min(int[] y) {
         int length = y.length;
-        if (length == 0) {
-            return Integer.MIN_VALUE;
-        }
         int[] x = new int[length];
-        for (int i = 0; i < length; ++i) {
-            x[i] = -y[i];
-        }
+        for (int i = 0; i < length; x[i] = -y[i], ++i);
         return -max(x);
     }
 
